41917f11cb31bde824c6a51cba22d0e88889aebc
Teerapong Manakul <imumi@msn.com>
2016-10-11 14:38:35 +0700

    split thai sentence

--- js/content_scripts.js
+++ js/content_scripts.js
@@ -1,7 +1,11 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+
     if (request.action == 'readit') {
         sendResponse({
             selection: window.getSelection().toString()
         });
     }
+
+    return true;
+
 });
